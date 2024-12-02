b67d552c04c076f245ecb8f6d445632fea5019b0
annika0209 <38184958+annika0209@users.noreply.github.com>
2024-12-02 12:26:12 +0900

    moon1202

---
+++ push.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "# annika0209      $0      $1" >> README.md
+#git init
+git add . #()
+git commit -m "$1"   #올릴때마다 히스토리(부연설명) 만들어줌
+#git branch -M main
+#git remote add origin https://github.com/annika0209/RISC_V  #깃과 연결
+git push -u origin main 
