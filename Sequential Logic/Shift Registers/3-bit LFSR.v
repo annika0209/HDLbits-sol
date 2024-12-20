b9798678fa5e22d3bb95e84290963fb95039374c
annika0209 <38184958+annika0209@users.noreply.github.com>
2024-12-20 21:49:50 +0900

    1220_2

--- Sequential Logic/Shift Registers/3-bit LFSR.v
+++ Sequential Logic/Shift Registers/3-bit LFSR.v
@@ -1,2 +1,34 @@
 
 //
+
+
+/*
+Write the Verilog code for this sequential circuit 
+(Submodules are ok, but the top-level must be named top_module).
+Assume that you are going to implement the circuit on the DE1-SoC board. Connect the R inputs to the SW switches, connect Clock to KEY[0], and L to KEY[1]. 
+Connect the Q outputs to the red lights LEDR.
+*/
+module top_module (
+	input [2:0] SW,      // R -> 3 switches
+	input [1:0] KEY,     // L and clk -> KEY[1] and KEY[0]
+	output [2:0] LEDR);  // Q -> a LED
+//There is no reset...
+
+wire    [2:0]  d;
+always @(posedge KEY[0]) begin
+    
+    LEDR <= d;
+end
+
+wire    tab =   LEDR[1]^LEDR[2];
+always @(*) begin
+    case (KEY[1])
+        0       :   d = {tab, LEDR[0], LEDR[2]}; 
+        1       :   d = SW;
+        default :   d = SW;  
+    endcase
+end
+
+
+endmodule
+
--- Sequential Logic/Shift Registers/32-bit LFSR.v
+++ Sequential Logic/Shift Registers/32-bit LFSR.v
@@ -1,30 +1,27 @@
 
 /*
-Write the Verilog code for this sequential circuit 
-(Submodules are ok, but the top-level must be named top_module).
-Assume that you are going to implement the circuit on the DE1-SoC board. Connect the R inputs to the SW switches, connect Clock to KEY[0], and L to KEY[1]. 
-Connect the Q outputs to the red lights LEDR.
+Build a 32-bit Galois LFSR with taps at bit positions 32, 22, 2, and 1.
 */
-module top_module (
-	input [2:0] SW,      // R -> 3 switches
-	input [1:0] KEY,     // L and clk -> KEY[1] and KEY[0]
-	output [2:0] LEDR);  // Q -> a LED
-//There is no reset...
+module top_module(
+    input clk,
+    input reset,    // Active-high synchronous reset to 32'h1
+    output [31:0] q
+); 
 
-wire    [2:0]  d;
-always @(posedge KEY[0]) begin
-    
-    LEDR <= d;
+wire    [31:0]  d;
+always @(posedge clk) begin
+    if (reset) begin
+        q <= 32'b1;
+    end
+    else q <= d; //else 빼먹음...
 end
 
-wire    tab =   LEDR[1]^LEDR[2];
-always @(*) begin
-    case (KEY[1])
-        0       :   d = {tab, LEDR[0], LEDR[2]}; 
-        1       :   d = SW;
-        default :   d = SW;  
-    endcase
-end
+wire    tab32 =   q[0]^1'b0;
+wire    tab22 =   q[0]^q[22];
+wire    tab2 =   q[0]^q[2];
+wire    tab1 =   q[1]^q[0];
+
+assign  d = { tab32, q[31:23],tab22 , q[21:3], tab2, tab1}; 
 
 
 endmodule
